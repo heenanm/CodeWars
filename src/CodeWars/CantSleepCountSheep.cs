8c54901ad11c38c526e235f4319aa1cf9332d7f1
Michael Heenan <mick.heenan@razor.co.uk>
2019-08-16 11:40:21 +0100

    Add Cant Sleep Count Sheep Kata

---
+++ src/CodeWars/CantSleepCountSheep.cs
@@ -0,0 +1,19 @@
+﻿using System.Text;
+
+namespace CodeWars
+{
+    public class CantSleepCountSheep
+    {
+        public static string CountSheep(int n)
+        {
+            StringBuilder sheepCount = new StringBuilder();
+
+            for (int i = 1; i <= n; i++)
+            {
+                sheepCount.Append(i.ToString() + " sheep...");
+            }
+
+            return sheepCount.ToString();
+        }
+    }
+}
---
+++ test/CodeWars.UnitTests/CantSleepCountSheepTests.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+
+namespace CodeWars.UnitTests
+{
+    public class CantSleepCountSheepTests
+    {
+        [TestCase(1, "1 sheep...")]
+        [TestCase(2, "1 sheep...2 sheep...")]
+        [TestCase(3, "1 sheep...2 sheep...3 sheep...")]
+        public void CountSheep(int n, string expected)
+        {
+            var actual = CantSleepCountSheep.CountSheep(n);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
