c10e8ab73bb8c46545fa93791a5412c46d1e0aff
Michael Heenan <mick.heenan@razor.co.uk>
2019-08-19 09:58:45 +0100

    Add Difference Of Volumes Of Cuboids

---
+++ src/CodeWars/DifferenceOfVolumesOfCuboids.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWars
+{
+    public class DifferenceOfVolumesOfCuboids
+    {
+        public static int FindDifference(int[] a, int[] b)
+        {
+            var aVolume = (a[0] * a[1] * a[2]);
+            var bVolume = (b[0] * b[1] * b[2]);
+
+            return Math.Max(aVolume, bVolume) - Math.Min(aVolume, bVolume);
+        }
+    }
+}
---
+++ test/CodeWars.UnitTests/DifferenceOfVolumesOfCuboidsTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+namespace CodeWars.UnitTests
+{
+    public class DifferenceOfVolumesOfCuboidsTests
+    {
+        [TestCase(new int[] { 3, 2, 5 }, new int[] { 1, 4, 4 }, 14)]
+        [TestCase(new int[] { 9, 7, 2 }, new int[] { 5, 2, 2 }, 106)]
+        [TestCase(new int[] { 11, 2, 5 }, new int[] { 1, 10, 8 }, 30)]
+        [TestCase(new int[] { 4, 4, 7 }, new int[] { 3, 9, 3 }, 31)]
+        [TestCase(new int[] { 15, 20, 25 }, new int[] { 10, 30, 25 }, 0)]
+        public void FindDifference(int[] a, int[] b, int expected)
+        {
+            var actual = DifferenceOfVolumesOfCuboids.FindDifference(a, b);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
