3944f1866c9796c3ce430b551f5eba6aae2e7a1c
Michael Heenan <mick.heenan@razor.co.uk>
2019-08-19 10:35:22 +0100

    Add Lost Without a Map

---
+++ src/CodeWars/LostWithoutAMap.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq;
+
+namespace CodeWars
+{
+    public class LostWithoutAMap
+    {
+        public static int[] Maps(int[] x)
+        {
+            var doubleX = x.Select(i => i * 2).ToArray();
+            return doubleX;
+        }
+    }
+}
---
+++ test/CodeWars.UnitTests/LostWithoutAMapTests.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+
+namespace CodeWars.UnitTests
+{
+    public class LostWithoutAMapTests
+    {
+        [TestCase(new[] { 1, 2, 3 }, new[] { 2, 4, 6 })]
+        [TestCase(new[] { 4, 1, 1, 1, 4 }, new[] { 8, 2, 2, 2, 8 })]
+        [TestCase(new[] { 2, 2, 2, 2, 2, 2 }, new[] { 4, 4, 4, 4, 4, 4 })]
+        public void Maps(int[] x, int[] expected)
+        {
+            var actual = LostWithoutAMap.Maps(x);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
