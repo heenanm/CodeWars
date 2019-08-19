685e2ef9ae085689ecb19d9f6941d4bcc51b98d0
Michael Heenan <mick.heenan@razor.co.uk>
2019-08-19 11:01:04 +0100

    You Only Need One

---
+++ src/CodeWars/YouOnlyNeedOne.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq;
+
+namespace CodeWars
+{
+    public class YouOnlyNeedOne
+    {
+        public static bool Check(object[] a, object x)
+        {
+            return a.Contains(x);
+        }
+    }
+}
---
+++ test/CodeWars.UnitTests/YouOnlyNeedOneTests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace CodeWars.UnitTests
+{
+    class YouOnlyNeedOneTests
+    {
+        [TestCase(new object[] { 66, 101 }, 66, true)]
+        [TestCase(new object[] { 80, 117, 115, 104, 45, 85, 112, 115 }, 45, true)]
+        [TestCase(new object[] { 't', 'e', 's', 't' }, 'e', true)]
+        [TestCase(new object[] { "what", "a", "great", "kata" },"kat" , false)]
+        public void Check(object[] a, object x, bool expected)
+        {
+            var actual = YouOnlyNeedOne.Check(a, x);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
