afaf7ab59c0e010b4d0aadb8802083ccc7d72632
Michael Heenan <mick.heenan@razor.co.uk>
2019-08-19 11:34:38 +0100

    Add Convert String To An Array

---
+++ src/CodeWars/ConvertStringToAnArray.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWars
+{
+    public class ConvertStringToAnArray
+    {
+        public static string[] StringToArray(string str)
+        {
+            string[] words = str.Split(' ');
+            return words;
+        }
+    }
+}
---
+++ test/CodeWars.UnitTests/ConvertStringToAnArrayTests.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+
+namespace CodeWars.UnitTests
+{
+    class ConvertStringToAnArrayTests
+    {
+        [TestCase("Robin Singh", new string[] { "Robin", "Singh" })]
+        [TestCase("I love arrays they are my favorite", new string[] { "I", "love", "arrays", "they", "are", "my", "favorite" })]
+        public void StringToArray(string str, string[] expected)
+        {
+            var actual = ConvertStringToAnArray.StringToArray(str);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
