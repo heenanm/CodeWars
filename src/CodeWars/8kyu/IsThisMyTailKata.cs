1512b28f0cc50c9aee7145b5f1cc44ce9fa7815f
Michael <Michael@DESKTOP-QLSCEJT>
2019-08-20 19:25:23 +0100

    Add Is This My Tail Kata

---
+++ src/CodeWars/8kyu/IsThisMyTailKata.cs
@@ -0,0 +1,16 @@
+﻿
+namespace CodeWars
+{
+    public class IsThisMyTailKata
+    { 
+        public static bool CorrectTail(string body, string tail)
+        {
+            var sub = body[body.Length - 1];
+
+            if (sub.ToString() == tail)
+                return true;
+            else
+                return false;
+        }
+    }
+}
---
+++ test/CodeWars.UnitTests/8kyu/IsThisMyTailTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace CodeWars.UnitTests
+{
+    public class IsThisMyTailTests
+    {
+        [TestCase("Fox", "x", true)]
+        [TestCase("Rhino", "o", true )]
+        [TestCase("Meerkat", "t", true)]
+        [TestCase("Emu", "t", false)]
+        [TestCase("Badger", "s", false)]
+        [TestCase("Giraffe", "d", false)]
+        public void CorrectTail(string body, string tail, bool expected)
+        {
+            var actual = IsThisMyTailKata.CorrectTail(body, tail);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
