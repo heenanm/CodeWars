127722ac2786b5f27eb30b4bc2f435f4eb2004e9
Michael Heenan <mick.heenan@razor.co.uk>
2019-08-29 13:39:23 +0100

    Add whitespace, add braces to for loop Count by X kata

--- src/CodeWars/8kyu/CountByXKata.cs
+++ src/CodeWars/8kyu/CountByXKata.cs
@@ -5,7 +5,11 @@
         public static int[] CountBy(int x, int n)
         {
             var countArray = new int[n];
-            for (var i = 0; i < n; i++) countArray[i] = (i + 1) * x;
+
+            for (var i = 0; i < n; i++)
+            {
+                countArray[i] = (i + 1) * x;
+            }
             return countArray;
         }
     }
