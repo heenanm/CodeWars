17607c5848a0555438b517af605fbddba6f9c2a4
Michael Heenan <mick.heenan@razor.co.uk>
2019-08-29 13:36:02 +0100

    Remove commented code Complementary DNA kata

--- src/CodeWars/8kyu/ComplementaryDnaKata.cs
+++ src/CodeWars/8kyu/ComplementaryDnaKata.cs
@@ -22,17 +22,5 @@ namespace CodeWars._8kyu
         {
             return string.Concat(dna.Select(x => _lookup[x]));
         }
-
-        //public static string MakeComplement(string dna)
-        //{
-        //    var temp = new StringBuilder();
-
-        //    for (var i = 0; i < dna.Length; i++)
-        //    {
-        //        temp.Append(_lookup[dna[i]]);               
-        //    }
-
-        //    return temp.ToString();
-        //}
     }
 }
